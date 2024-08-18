649a97e1a56c071ebf4e3cd645b21ac11219c0c0
Sergio Vergara <sergiovergara123@gmail.com>
2024-08-17 19:55:57 -0500

    git init, built-in data structures

---
+++ codevolution/01-array.js
@@ -0,0 +1,11 @@
+const arr = [1, 2, 3, 'Sergio', true]
+arr. push (4)
+arr.unshift(0)
+arr.pop()
+arr.shif()
+
+for (const item of arr) {
+console. log (item)
+}
+
+// map, filter, reduce, concat, slice and splice
---
+++ codevolution/04-map.js
@@ -0,0 +1,16 @@
+const map = new Map([
+  ["a", 1],
+  ["b", 2],
+]);
+
+map.set("c", 3);
+map.delete("c");
+console.log(map.has("a"));
+
+console.log(map.size);
+
+map.clear();
+
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
